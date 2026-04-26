8631e38a0c5c23893a322095432019890bb098ca
Abdelrhman Sayed <abdelrhman.sayed.abobakr@gmail.com>
2026-04-26 16:57:48 +0300

    Delete clear_data.py

--- clear_data.py
+++
@@ -1,8 +0,0 @@
-import sqlite3, os
-conn = sqlite3.connect(os.environ.get('DATABASE_PATH', 'trip_tracker.db'))
-for tbl in ['trips', 'workshop_records', 'emergency_reports', 'garage_records']:
-    conn.execute(f'DELETE FROM {tbl}')
-    print(f'✓ {tbl} cleared')
-conn.commit()
-conn.close()
-print('Done!')
