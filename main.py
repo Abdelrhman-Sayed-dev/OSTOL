3db12f90a50a07d0c12a73f2ac672df4501b8016
Abdelrhman Sayed <abdelrhman.sayed.abobakr@gmail.com>
2026-07-01 13:15:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 from datetime import datetime, timedelta
 from contextlib import contextmanager
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 
 try:
     import openpyxl
